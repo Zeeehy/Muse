57d04958cb8d4bbf88ae3d978396891f78fde597
microsoft\SIST <jdy04158@naver.com>
2024-11-21 18:41:30 +0900

    [feat] 뮤즈캘린더 수정중

--- muse/src/main/java/com/muse/myPage/model/MuseCalendarDAOImpl.java
+++ muse/src/main/java/com/muse/myPage/model/MuseCalendarDAOImpl.java
@@ -1,5 +1,7 @@
 package com.muse.myPage.model;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,5 +14,16 @@ public class MuseCalendarDAOImpl implements MuseCalendarDAO {
 		super();
 		this.sqlMap = sqlMap;
 	}
+	
+	@Override
+	public List<MuseCalendarDTO> getCalendarActorCast(String u_id) {
+		List<MuseCalendarDTO> calendarActorCast =sqlMap.selectList("selectCalendarActorList",u_id);
+		return calendarActorCast;
+	}
 
+	@Override
+	public List<MuseCalendarDTO> getCalendarBooking(String u_id) {
+		List<MuseCalendarDTO> calendarBooking =sqlMap.selectList("selectCalendarBookingList",u_id);
+		return calendarBooking;
+	}
 }
